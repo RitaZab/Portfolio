66cd58b4230398dc110462c742732b0813bba9ec
Rita Zaborowska <rm.zaborowska@gmail.com>
2022-12-19 18:48:47 +0100

    index-ading rotate to main icon

---
+++ index.js
@@ -0,0 +1,7 @@
+let toggleIcon = document.querySelector("main_icon")
+let toggleMenu = document.getElementById("toggle_menu")
+
+toggleMenu.onclick = function () {
+    toggleMenu.classList.toggle("active")
+}
+console.log(toggleMenu)
